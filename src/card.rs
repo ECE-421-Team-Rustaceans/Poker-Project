4f80930eadccc78f80fae38c0a37fc4c1276b2bc
Cedric Boucher <onebitalpha@gmail.com>
2025-02-01 12:39:31 -0700

    add documentation to Card class

--- src/card.rs
+++ src/card.rs
@@ -4,12 +4,23 @@ mod suit;
 pub use suit::Suit;
 
 #[derive(Debug)]
+/// Card class, containing a rank and a suit.
+/// Create a new card with Card::new(),
+/// Example:
+/// ```
+/// let card = Card::new(Rank::Ace, Suit::Spades);
+/// ```
 pub struct Card {
     rank: Rank,
     suit: Suit
 }
 
 impl Card {
+    /// Constructor for Card.
+    /// Example:
+    /// ```
+    /// let card = Card::new(Rank::Ace, Suit::Spades);
+    /// ```
     pub fn new(rank: Rank, suit: Suit) -> Card {
         let card = Card {
             rank,
@@ -18,10 +29,12 @@ impl Card {
         return card;
     }
 
+    /// Get the Rank of this Card
     pub fn rank(&self) -> &Rank {
         return &self.rank;
     }
 
+    /// Get the Suit of this Card
     pub fn suit(&self) -> &Suit {
         return &self.suit;
     }
