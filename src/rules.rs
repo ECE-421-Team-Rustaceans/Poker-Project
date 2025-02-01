883d8cc47b656cbaf0ce5cde5eae4a02ab913cae
Cedric Boucher <onebitalpha@gmail.com>
2025-02-01 13:14:08 -0700

    create rules trait

---
+++ src/rules.rs
@@ -0,0 +1,5 @@
+/// trait containing necessary methods for each set of poker Rules
+pub trait Rules {
+    /// the play_round method takes care of all of the logic required the entire game, for a given variant of poker
+    fn play_game(&self);
+}
