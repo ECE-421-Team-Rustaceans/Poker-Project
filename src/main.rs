043e3eb372c7f3945ce5b9d0e568f8a690476290
Cedric Boucher <onebitalpha@gmail.com>
2025-02-01 12:54:52 -0700

    add some code to main to demonstrate new methods

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,6 @@ fn main() {
     println!("card suit: {:?}", card.suit());
     println!("card is number: {:?}", card.is_number());
     println!("card is face: {:?}", card.is_face());
+    println!("card is black {:?}", card.is_black());
+    println!("card is red {:?}", card.is_red());
 }
