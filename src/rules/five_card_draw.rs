637f69d819cc8cf53d233c2ef8f43e50f33b73b0
Cedric Boucher <onebitalpha@gmail.com>
2025-02-09 15:25:00 -0700

    set up basic two-round method structure

--- src/rules/five_card_draw.rs
+++ src/rules/five_card_draw.rs
@@ -4,8 +4,19 @@ pub struct FiveCardDraw {
 
 }
 
+impl FiveCardDraw {
+    fn play_round_one(&self) {
+        todo!()
+    }
+
+    fn play_round_two(&self) {
+        todo!()
+    }
+}
+
 impl Rules for FiveCardDraw {
     fn play_game(&self) {
-        todo!()
+        self.play_round_one();
+        self.play_round_two();
     }
 }
