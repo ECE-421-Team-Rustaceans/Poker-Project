bf606aca78a890485ea084d3ace5a09435af28da
Cedric Boucher <onebitalpha@gmail.com>
2025-02-09 16:05:42 -0700

    add new method and deal_initial_cards method

--- src/rules/five_card_draw.rs
+++ src/rules/five_card_draw.rs
@@ -1,10 +1,21 @@
+use crate::deck::Deck;
 use super::Rules;
+use crate::player::Player;
 
-pub struct FiveCardDraw {
-
+pub struct FiveCardDraw<'a> {
+    players: Vec<&'a Player>,
+    deck: Deck
 }
 
-impl FiveCardDraw {
+impl<'a> FiveCardDraw<'a> {
+    fn new(players: Vec<&Player>) -> FiveCardDraw {
+        let deck = Deck::new();
+        return FiveCardDraw {
+            players,
+            deck
+        };
+    }
+
     fn play_round_one(&self) {
         todo!()
     }
@@ -12,9 +23,16 @@ impl FiveCardDraw {
     fn play_round_two(&self) {
         todo!()
     }
+
+    fn deal_initial_cards(&self) -> Result<(), String> {
+        for player in self.players {
+            player.obtain_card(self.deck.deal()?);
+        }
+        return Ok(());
+    }
 }
 
-impl Rules for FiveCardDraw {
+impl<'a> Rules for FiveCardDraw<'a> {
     fn play_game(&self) {
         self.play_round_one();
         self.play_round_two();
