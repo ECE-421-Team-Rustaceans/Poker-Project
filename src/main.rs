46d4850cd0a98099a3bcaeb62089cb24fea329e4
katzhong <kathari2@ualberta.ca>
2025-02-10 15:52:58 -0700

    uncommented tests in main

--- src/main.rs
+++ src/main.rs
@@ -15,71 +15,25 @@ mod action_option;
 
 
 fn main() {
-    // let mut deck = Deck::new();
-    // println!("{deck:#?}");
-    // let card = deck.deal().unwrap();
-    // println!("{card:#?}");
-    // println!("card rank: {:?}", card.rank());
-    // println!("card suit: {:?}", card.suit());
-    // println!("card is number: {:?}", card.is_number());
-    // println!("card is face: {:?}", card.is_face());
-    // println!("card is black {:?}", card.is_black());
-    // println!("card is red {:?}", card.is_red());
-    // deck.return_card(card);
-    // println!("{deck:#?}");
+    println!("poker time");
+
+    let mut deck = Deck::new();
+    println!("{deck:#?}");
+    let card = deck.deal().unwrap();
+    println!("{card:#?}");
+    println!("card rank: {:?}", card.rank());
+    println!("card suit: {:?}", card.suit());
+    println!("card is number: {:?}", card.is_number());
+    println!("card is face: {:?}", card.is_face());
+    println!("card is black {:?}", card.is_black());
+    println!("card is red {:?}", card.is_red());
+    deck.return_card(card);
+    println!("{deck:#?}");
     // let test = FiveCardDraw {};
     // test.play_game();
     // let test = SevenCardDraw {};
     // test.play_game();
     // let test = KansasCityLowball {};
     // test.play_game();
-
-    println!("poker time");
-
-    // TODO: implement error checking for out of bounds entries
-    println!("enter number of players (2-10):");
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("failed to read line");
-    let num_players: usize = input
-        .trim()
-        .parse()
-        .expect("not an integer");
-
-    println!("\nenter names in playing order:");
-
-    let mut players: Vec<String> = Vec::new();
-
-    for i in 1..=num_players {
-        let mut input = String::new();
-        println!("enter name for player {}: ", i);
-        io::stdin()
-            .read_line(&mut input)
-            .expect("failed to read line");
-        let name = input
-            .trim()
-            .to_string();
-        players.push(name);
-    }
-
-    println!("\nplayers:");
-    for (index, player) in players.iter().enumerate() {
-        println!("player {}: {}", index + 1, player);
-    }
-
-    println!("\nselect a game:\n1 - five card draw\n2 - seven card draw\n3 - kansas city lowball");
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("failed to read line");
-    let game: u32 = input
-        .trim()
-        .parse()
-        .expect("not an integer");
-
-    // enter either name or username to look at stats
-    // convert integer game option to game to send to game, or have game handle it
-    // something like Game::play_game(players, game)
 
 }
