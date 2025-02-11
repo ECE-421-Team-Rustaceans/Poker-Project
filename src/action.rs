f96709c891d495fdb2bb11c411d6e46925a184be
James Fu <zejia@ualberta.ca>
2025-02-10 21:59:46 -0700

    Implemented DbHandler with tests on get, add, and delete operations

--- src/action.rs
+++ src/action.rs
@@ -1,3 +1,5 @@
+use serde::{ Deserialize, Serialize };
+
 /// Player Action enum
 /// 
 /// This contains action types that players can do throuhgout a game.
@@ -16,6 +18,7 @@
 /// 
 /// Win and Lose actions are for book keeping and will be added onto the pot history
 /// after dividing the winnings for a particular round as turns in a separte phase.
+#[derive(Serialize, Deserialize, Debug)]
 pub enum Action {
     Ante(usize),
     Call,
--- src/database/db_structs.rs
+++ src/database/db_structs.rs
@@ -1,3 +1,5 @@
+use serde::{ Deserialize, Serialize, Serializer, ser::SerializeSeq };
+use uuid::serde::simple;
 use uuid::Uuid;
 
 use crate::card::Card;
@@ -9,6 +11,7 @@ use crate::action::Action;
 /// Below are the supported poker game types by this server. Other game
 /// types may be added in the future. Currently, we only support draw
 /// style poker.
+#[derive(Serialize, Deserialize, Debug)]
 pub enum GameType {
     FiveCardDraw,
     SevenCardDraw,
@@ -20,9 +23,24 @@ pub enum GameType {
 /// Information about one "poker game" (i.e. playing until there are insufficient players to play)
 /// is held in this struct. game_id is used to identify unique games and to relate them to 
 /// "rounds" within the database.
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Game {
-    game_id: Uuid,
+    #[serde(with = "uuid::serde::simple")]
+    _id: Uuid,
     game_type: GameType,
+}
+
+
+fn simple_uuids<S: Serializer>(uuid_list: &Vec<Uuid>, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let mut seq = s.serialize_seq(Some(uuid_list.len()))?;
+    for id in uuid_list {
+        let id_str = id.simple().to_string();
+        seq.serialize_element(&id_str)?;
+    }
+    return seq.end();
 }
 
 /// Round struct
@@ -31,10 +49,15 @@ pub struct Game {
 /// in this struct. Each round has a round_id along with a game_id it is associated with.
 /// Rounds are comprised of player turns and are associated to a group of players that
 /// played in that round. Players should not be able to join mid round.
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Round {
+    #[serde(with = "uuid::serde::simple")]
+    _id: Uuid,
+    #[serde(with = "uuid::serde::simple")]
     game_id: Uuid,
-    round_id: Uuid,
+    #[serde(serialize_with = "simple_uuids")]
     turn_ids: Vec<Uuid>,
+    #[serde(serialize_with = "simple_uuids")]
     player_ids: Vec<Uuid>,
 }
 
@@ -46,10 +69,14 @@ pub struct Round {
 /// along with a round_id it is associated with. A player ID and their associated hand before 
 /// their action is also stored in this struct. These turns are also grouped together
 /// by "phases". The number of phases depends on the game type. 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Turn {
+    #[serde(with = "uuid::serde::simple")]
+    pub _id: Uuid,
+    #[serde(with = "uuid::serde::simple")]
     pub round_id: Uuid,
-    pub turn_id: Uuid,
     pub phase_num: usize,
+    #[serde(with = "uuid::serde::simple")]
     pub acting_player_id: Uuid,
     pub hand: Vec<Card>,
     pub action: Action,
@@ -60,6 +87,8 @@ pub struct Turn {
 /// These are recognized accounts on our system. Each account has a unique ID along
 /// with any personal information. To play poker games on this server, you must
 /// have an account on our system.
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Account {
-    account_id: Uuid,
+    #[serde(with = "uuid::serde::simple")]
+    pub _id: Uuid,
 }
