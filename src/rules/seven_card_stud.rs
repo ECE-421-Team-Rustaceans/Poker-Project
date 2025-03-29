23b27586b3383acfe5403059aef4b706b1f5538d
Cedric Boucher <onebitalpha@gmail.com>
2025-03-29 13:39:05 -0600

    update trait impl in seven card stud

--- src/rules/seven_card_stud.rs
+++ src/rules/seven_card_stud.rs
@@ -1,11 +1,12 @@
+use crate::player::Player;
 use super::Rules;
 
 pub struct SevenCardStud {
 
 }
 
-impl Rules for SevenCardStud {
-    fn play_round(&mut self) {
+impl<'a> Rules<'a> for SevenCardStud {
+    fn play_round(&mut self, players: Vec<&'a mut Player>) -> Result<(), &'static str> {
         todo!()
     }
 }
