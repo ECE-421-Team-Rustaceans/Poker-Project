0b31eabd6387580a83ecc0b06fe022d5131cdd07
Cedric Boucher <onebitalpha@gmail.com>
2025-03-30 14:08:01 -0600

    rename remaining unimplemented game to texas holdem

--- src/rules/texas_holdem.rs
+++ src/rules/texas_holdem.rs
@@ -1,11 +1,11 @@
 use crate::player::Player;
 use super::Rules;
 
-pub struct KansasCityLowball {
+pub struct TexasHoldem {
 
 }
 
-impl<'a> Rules<'a> for KansasCityLowball {
+impl<'a> Rules<'a> for TexasHoldem {
     fn play_round(&mut self, players: Vec<&'a mut Player>) -> Result<(), &'static str> {
         todo!()
     }
